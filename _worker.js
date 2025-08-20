09f4e78122dd9daf57ecfef4315a30be3cc96be6
gh-bibica-net <github@bibica.net>
2025-08-20 10:55:03 +0700

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,23 @@
+export default {
+    async fetch(request, env) {
+        let url = new URL(request.url);
+        
+        // Proxy tất cả requests đến GitHub
+        if (url.pathname !== '/') {
+            // Tạo URL đích
+            let targetUrl = new URL(`https://github.com/bibicadotnet${url.pathname}${url.search}`);
+            
+            // Tạo request mới với headers gốc
+            let newRequest = new Request(targetUrl, {
+                method: request.method,
+                headers: request.headers,
+                body: request.body
+            });
+            
+            return fetch(newRequest);
+        }
+        
+        // Serve static assets cho root path
+        return env.ASSETS.fetch(request);
+    }
+};
